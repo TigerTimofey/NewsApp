bc7870e5e9d1658e92bf2a4e857b4dbda9892074
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2023-07-07 23:07:46 +0300

    dateStart, dateEnd, lang and sumbit it

--- src/Body/SearchForm.jsx
+++ src/Body/SearchForm.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 function SearchForm({ handleClose }) {
+  const [articlesSortDisabled, setArticlesSortDisabled] = useState(false);
+
   const resultType = [
     "articles",
     "uriWgtList",
@@ -28,11 +31,46 @@ function SearchForm({ handleClose }) {
     "socialScore",
     "facebookShares",
   ];
+  const languages = [
+    {
+      label: "English",
+      value: "eng",
+    },
+    {
+      label: "Estonian",
+      value: "est",
+    },
+    { label: "Russian", value: "rus" },
+    { label: "Georgian", value: "kat" },
+  ];
   const dataType = ["news", "pr", "blog"];
   const handleSumbit = (event) => {
     event.preventDefault();
-    console.log("event", event);
+    console.log("event", event.target.keyword.value);
+    const data = {
+      keyword: event.target.keyword.value,
+      resultType: event.target.resultType.value,
+      articlesSortBy: event.target.articlesSortBy.value,
+      dataType: [...event.target.dataType]
+        .filter((e) => e.checked)
+        .map((d) => d.value),
+      lang: [...event.target.lang]
+        .filter((e) => e.selected)
+        .map((d) => d.value),
+      dateStart: event.target.dateStart.value,
+      dateEnd: event.target.dateEnd.value,
+    };
+    console.log("data", data);
+    handleClose();
   };
+  const handleResultTypeChange = (event) => {
+    if (event.target.value !== "articles") {
+      setArticlesSortDisabled(true);
+    } else {
+      setArticlesSortDisabled(false);
+    }
+  };
+
   return (
     <Form onSubmit={handleSumbit}>
       <Form.Group className="mb-3">
@@ -42,8 +80,11 @@ function SearchForm({ handleClose }) {
 
       <Form.Group>
         <Form.Label>Result type</Form.Label>
-
-        <Form.Select className="mb-3" name="resultType">
+        <Form.Select
+          className="mb-3"
+          name="resultType"
+          onChange={handleResultTypeChange}
+        >
           {resultType.map((type) => (
             <option value={type} key={type}>
               {type}
@@ -52,17 +93,33 @@ function SearchForm({ handleClose }) {
         </Form.Select>
         <Form.Label>Arcticles sort by</Form.Label>
 
-        <Form.Select className="mb-3" name="articlesSortBy">
+        <Form.Select
+          className="mb-3"
+          name="articlesSortBy"
+          disabled={articlesSortDisabled}
+        >
           {articlesSortBy.map((type) => (
             <option value={type} key={type}>
               {type}
             </option>
           ))}
         </Form.Select>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Date start</Form.Label>
+          <Form.Control type="date" name="dateStart" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Date end</Form.Label>
+          <Form.Control type="date" name="dateEnd" />
+        </Form.Group>
+
         <Form.Group className="mb-3">
           <Form.Label>Data type</Form.Label>
           {dataType.map((type) => (
             <Form.Check
+              value={type}
               type="checkbox"
               label={type}
               key={type}
@@ -70,6 +127,17 @@ function SearchForm({ handleClose }) {
             ></Form.Check>
           ))}
         </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Language</Form.Label>
+          <Form.Select name="lang" multiple>
+            {languages.map(({ value, label }) => (
+              <option value={value} key={value}>
+                {label}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+
         <Button type="submit" variant="outline-danger">
           Close sidebar
         </Button>
