d0d964dbcd728883447f0d362a71999aacad2d2e
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2023-07-17 21:38:20 +0300

    ScrollToTop

--- src/Body/index.jsx
+++ src/Body/index.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import News from "./News";
 import SideBar from "./SideBar";
+import Button from "react-bootstrap/Button";
 function Body() {
   const [newsList, setNewsList] = useState(null);
   const [info, setInfo] = useState(null);
+  const scrollToTop2 = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <>
@@ -14,7 +21,14 @@ function Body() {
         info={info}
         setInfo={setInfo}
       />
+
+      <div className="ScrollToTop">
+        <Button variant="light" onClick={scrollToTop2}>
+          <h4>🔝</h4>
+        </Button>
+      </div>
     </>
   );
 }
+
 export default Body;
